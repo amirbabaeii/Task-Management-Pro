f3960980b5c8fc903064ab7a3bb8f8ce3a3f42c2
Amir Babaei <amirbabaei.dev@gmail.com>
2024-11-12 01:09:04 +0330

    Feat:  added task repository and its interface

--- app/Repositories/Interfaces/TaskRepositoryInterface.php
+++ app/Repositories/Interfaces/TaskRepositoryInterface.php
@@ -2,11 +2,16 @@
 
 namespace App\Repositories\Interfaces;
 
+use App\Models\Task;
+use Illuminate\Contracts\Pagination\LengthAwarePaginator;
+use Illuminate\Database\Eloquent\Collection;
+
 interface TaskRepositoryInterface
 {
-    public function create(array $data);
-    public function update(array $data, $id);
-    public function delete($id);
-    public function find($id);
+    public function list(int $limit) : Collection|LengthAwarePaginator;
+    public function create(array $data) : Task;
+    public function update(array $data, $id) : Task;
+    public function delete($id) : bool;
+    public function find($id) : Task;
 
-} 
+}
--- app/Repositories/TaskRepository.php
+++ app/Repositories/TaskRepository.php
@@ -5,27 +5,34 @@ namespace App\Repositories;
 use App\Models\Task;
 use App\Repositories\Interfaces\TaskRepositoryInterface;
 use App\Repositories\Interfaces\UserRepositoryInterface;
+use Illuminate\Contracts\Pagination\LengthAwarePaginator;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Support\Facades\Hash;
 
 class TaskRepository implements TaskRepositoryInterface
 {
-    public function create(array $data)
+    public function list(int $limit): Collection|LengthAwarePaginator
+    {
+        return Task::paginate($limit);
+    }
+    public function create(array $data): Task
     {
         return Task::create($data);
     }
 
-    public function delete($id)
+    public function delete($id): bool
     {
         return Task::destroy($id);
     }
 
-    public function find($id)
+    public function find($id): Task
     {
         return Task::find($id);
     }
 
-    public function update(array $data, $id)
+    public function update(array $data, $id): Task
     {
         return Task::find($id)->update($data);
     }
-} 
+
+}
