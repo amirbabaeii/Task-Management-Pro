ce2b8950563c01837afed47e827fb1b874abe0c4
Amir Mohammad Babaei <amirbabaei.dev@gmail.com>
2024-11-25 02:06:10 +0330

    Refactor: changed order of parameters for better consistency

--- app/Repositories/Interfaces/TaskRepositoryInterface.php
+++ app/Repositories/Interfaces/TaskRepositoryInterface.php
@@ -10,7 +10,7 @@ interface TaskRepositoryInterface
 {
     public function list(int $limit) : Collection|LengthAwarePaginator;
     public function create(array $data) : Task;
-    public function update(array $data, $id) : Task;
+    public function update($id, array $data) : Task;
     public function delete($id) : bool;
     public function find($id) : Task;
 
