0fb7c5e2f96a9f3e142b5ca45188c1b15f68dbea
Amir Mohammad Babaei <amirbabaei.dev@gmail.com>
2025-09-28 01:22:26 +0200

    Adjust task resource pagination payload

--- app/Http/Resources/TaskResource.php
+++ app/Http/Resources/TaskResource.php
@@ -2,19 +2,36 @@
 
 namespace App\Http\Resources;
 
-use Illuminate\Http\Resources\Json\JsonResource;
-use App\Http\Resources\ApiResource;
+use Illuminate\Contracts\Pagination\LengthAwarePaginator;
 
 class TaskResource extends ApiResource
 {
     public function __construct($resource, $message, $statusCode)
     {
-        parent::__construct( $resource, $message, $statusCode);
+        parent::__construct($resource, $message, $statusCode);
     }
-    
+
     public function toArray($request)
     {
-        return parent::toArray($request);
+        $response = parent::toArray($request);
+
+        if ($this->resource instanceof LengthAwarePaginator) {
+            $response['data'] = $this->transformPaginatedData($this->resource);
+        }
+
+        return $response;
+    }
+
+    private function transformPaginatedData(LengthAwarePaginator $paginator): array
+    {
+        $paginated = $paginator->toArray();
+
+        $paginated['data'] = array_map(
+            static fn (array $task) => $task + ['data' => $task],
+            $paginated['data']
+        );
+
+        return $paginated;
     }
 }
 
